bc6cf80ec7108245e83fee10efaa90767aaf41f1
wangtengteng <1751727782@qq.com>
2021-02-20 19:19:17 +0800

    first

---
+++ cloudfunctions/login/index.js
@@ -0,0 +1,16 @@
+const cloud = require('wx-server-sdk');
+console.log(222)
+
+cloud.init();
+
+exports.main = (event, context) => {
+    console.log(event)
+    console.log(context)
+  
+    // 可执行其他自定义逻辑
+    // console.log 的内容可以在云开发云函数调用日志查看
+  
+    return {
+      openid: event.userInfo.openId,
+    }
+}
